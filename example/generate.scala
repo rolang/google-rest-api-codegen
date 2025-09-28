edfd0a5baff60e8274b079cc4387a950eef69a77
Roman Langolf <rolang@pm.me>
2025-09-28 14:12:36 +0700

    update example

--- example/generate.scala
+++ example/generate.scala
@@ -1,17 +1,16 @@
-//> using scala 3.7.0
-//> using dep dev.rolang::gcp-codegen::0.0.5
+//> using scala 3.7.3
+//> using dep dev.rolang::gcp-codegen::0.0.8
 
 import gcp.codegen.*, java.nio.file.*, GeneratorConfig.*
 
 @main def run =
   val files = Task(
-    specsInput = SpecsInput.FilePath(Path.of("pubsub_v1.json")),
+    specsInput = SpecsInput.StdIn,
     config = GeneratorConfig(
       outDir = Path.of("out"),
       outPkg = "example.pubsub.v1",
       httpSource = HttpSource.Sttp4,
       jsonCodec = JsonCodec.Jsoniter,
-      dialect = Dialect.Scala3,
       arrayType = ArrayType.List,
       preprocess = specs => specs
     )
